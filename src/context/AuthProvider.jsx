1ac748dc3f5df5a821a7587838d62a21294011bc
jeanmo333 <jeanmo333@gmail.com>
2022-07-14 00:54:55 -0400

    commit

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,122 +1,120 @@
-import { useState, useEffect, createContext  } from 'react'
-import clienteAxios from '../config/axios'
+import { useState, useEffect, createContext } from "react";
+import clienteAxios from "../config/axios";
+import { useNavigate } from "react-router-dom";
 
-const AuthContext = createContext()
-const AuthProvider = ({children}) => {
+const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const [cargando, setCargando] = useState(true);
+  const [auth, setAuth] = useState({});
+  const navigate = useNavigate();
 
-    const [cargando, setCargando] = useState(true)
-    const [ auth, setAuth ] = useState({})
+  useEffect(() => {
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        setCargando(false);
+        return;
+      }
 
-    useEffect(() => {
-        const autenticarUsuario = async () => {
-            const token = localStorage.getItem('token')
-            if(!token) {
-                setCargando(false)
-                return
-            }
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      try {
+        const { data } = await clienteAxios("/veterinarios/perfil", config);
+        setAuth(data);
+        navigate("/admin");
+      } catch (error) {
+        console.log(error.response.data.msg);
+        setAuth({});
+      }
 
-            const config = {
-                headers: {
-                    "Content-Type": "application/json", 
-                    Authorization: `Bearer ${token}`
-                }
-            }
-            try {
-                const { data } = await clienteAxios('/veterinarios/perfil', config)
-                setAuth(data)
-            } catch (error) {
-                console.log(error.response.data.msg)
-                setAuth({})
-            }
+      setCargando(false);
+    };
+    autenticarUsuario();
+  }, []);
 
-            setCargando(false)
-            
-        }
-        autenticarUsuario()
-    }, [])
+  const cerrarSesion = () => {
+    localStorage.removeItem("token");
+    setAuth({});
+  };
 
-    const cerrarSesion = () => {
-        localStorage.removeItem('token')
-        setAuth({})
+  const actualizarPerfil = async (datos) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setCargando(false);
+      return;
     }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
 
-    const actualizarPerfil = async datos => {
-        const token = localStorage.getItem('token')
-        if(!token) {
-            setCargando(false)
-            return
-        }
-        const config = {
-            headers: {
-                "Content-Type": "application/json", 
-                Authorization: `Bearer ${token}`
-            }
-        }
+    try {
+      const url = `/veterinarios/perfil/${datos._id}`;
+      await clienteAxios.put(url, datos, config);
 
-        try {
-            const url = `/veterinarios/perfil/${datos._id}`
-            await clienteAxios.put(url, datos, config)
-
-            return {
-                msg: 'Almacenado Correctamente'
-            }
-        } catch (error) {
-            return {
-                msg: error.response.data.msg,
-                error: true
-            }
-        }
+      return {
+        msg: "Almacenado Correctamente",
+      };
+    } catch (error) {
+      return {
+        msg: error.response.data.msg,
+        error: true,
+      };
     }
+  };
 
-    const guardarPassword = async (datos) => {
-        const token = localStorage.getItem('token')
-        if(!token) {
-            setCargando(false)
-            return
-        }
-        const config = {
-            headers: {
-                "Content-Type": "application/json", 
-                Authorization: `Bearer ${token}`
-            }
-        }
-
-        try {
-            const url = '/veterinarios/actualizar-password'
-
-            const { data } = await clienteAxios.put(url, datos, config)
-            console.log(data) 
-
-            return {
-                msg: data.msg
-            }
-        } catch (error) {
-            return {
-                msg: error.response.data.msg,
-                error: true
-            }
-        }
-
+  const guardarPassword = async (datos) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setCargando(false);
+      return;
     }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
 
-    return(
-        <AuthContext.Provider
-            value={{
-                auth, 
-                setAuth,
-                cargando, 
-                cerrarSesion,
-                actualizarPerfil,
-                guardarPassword
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
-}
+    try {
+      const url = "/veterinarios/actualizar-password";
 
-export {
-    AuthProvider
-}
+      const { data } = await clienteAxios.put(url, datos, config);
+      console.log(data);
 
-export default AuthContext
+      return {
+        msg: data.msg,
+      };
+    } catch (error) {
+      return {
+        msg: error.response.data.msg,
+        error: true,
+      };
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+        cargando,
+        cerrarSesion,
+        actualizarPerfil,
+        guardarPassword,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider };
+
+export default AuthContext;
